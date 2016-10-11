3111ad96da04e45a4630dd0e1a95032ab8b596e7
SpaceToastCoatToCoast <alloalloalan@gmail.com>
2016-10-10 18:29:38 -1000

    added products database

---
+++ db/products.js
@@ -0,0 +1,58 @@
+module.exports = (function(){
+
+  let allProducts = [];
+
+  function _all() {
+    return allProducts;
+  }
+
+  function _add(product) {
+    if(product.name === undefined) {
+      return false;
+    }
+    allPosts.push(product);
+    return true;
+  }
+
+  function _getById(product) {
+    let [foundProduct] = allProducts.filter((product) => {
+      return product.id === id;
+    });
+    console.log('in getProduct');
+    return foundProduct;
+  }
+
+  function _editById(id, changedData) {
+    let productToEdit = _getById(id);
+    let productIndex = allProducts.indexOf(productToEdit);
+    if(productIndex > -1) {
+      if(changedData.hasOwnProperty('name')) {
+        productToEdit.name = changedData.name;
+      }
+      if(changedData.hasOwnProperty('price')) {
+        productToEdit.price = changedData.price;
+      }
+      if(changedData.hasOwnProperty('inventory')) {
+        productToEdit.inventory = changedData.inventory;
+      }
+    }
+  }
+
+  function _deleteById(id) {
+    let productIndex = allProducts.indexOf(_getById(id));
+    if(productIndex > -1) {
+      allProducts.splice(productIndex, 1);
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  return {
+    all: _all,
+    add: _add,
+    getById: _getById,
+    deleteById: _deleteById,
+    editById: _editById
+  };
+})();
